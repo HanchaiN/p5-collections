0b2fb1d39fd7a885b82967b539df9237ecb1334d
Hanchai Nonprasart <85230240+HanchaiN@users.noreply.github.com>
2023-08-30 21:39:14 +0700

    fixup

--- src/pages/creative_coding/ray_tracing/index.tsx
+++ src/pages/creative_coding/ray_tracing/index.tsx
@@ -1,6 +1,10 @@
 import Header from "@/components/header";
 import { previewContainer, sketch } from "@/styles/creative_coding.module.css";
-import { headlineLarge } from "@/styles/main.module.css";
+import {
+  bodyMedium,
+  headlineLarge,
+  textContainer,
+} from "@/styles/main.module.css";
 import React, { Suspense, useEffect, useRef } from "react";
 
 const Preview = React.lazy(async () => {
@@ -43,6 +47,28 @@ export default function Body() {
         <Suspense>
           <Preview />
         </Suspense>
+        <p className={`${textContainer} ${bodyMedium}`}>
+          In 3D computer graphics, ray tracing is a technique for modeling light
+          transport for use in a wide variety of rendering algorithms for
+          generating digital images. On a spectrum of computational cost and
+          visual fidelity, ray tracing-based rendering techniques, such as ray
+          casting, recursive ray tracing, distribution ray tracing, photon
+          mapping and path tracing, are generally slower and higher fidelity
+          than scanline rendering methods. Thus, ray tracing was first deployed
+          in applications where taking a relatively long time to render could be
+          tolerated, such as still computer-generated images, and film and
+          television visual effects (VFX), but was less suited to real-time
+          applications such as video games, where speed is critical in rendering
+          each frame. Ray tracing is capable of simulating a variety of optical
+          effects, such as reflection, refraction, soft shadows, scattering,
+          depth of field, motion blur, caustics, ambient occlusion and
+          dispersion phenomena (such as chromatic aberration). It can also be
+          used to trace the path of sound waves in a similar fashion to light
+          waves, making it a viable option for more immersive sound design in
+          video games by rendering realistic reverberation and echoes. In fact,
+          any physical wave or particle phenomenon with approximately linear
+          motion can be simulated with ray tracing.
+        </p>
       </article>
     </>
   );
@@ -51,7 +77,7 @@ export default function Body() {
 export function Head() {
   return (
     <>
-      <Header title="Boids" />
+      <Header title="Ray Tracing" />
     </>
   );
 }
